7bff7c408e4a945658d8405ddfca309e25502d35
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-01 15:21:27 +0530

    Update App.jsx
    Updated App.jsx file

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -1,15 +1,19 @@
 // import logo from './logo.svg';
 import './App.css';
-import ClassComponent from './components/ClassComponent';
+import PropsComponent from './components/Day2/PropComponent';
+// import FunctionComponent from './components/Day2/FunctionComponent';
+// import ClassComponent from './components/Day2/ClassComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>
-           <ClassComponent/>
+         <h1>
+          hello
+           {/* <ClassComponent/> */}
+           {/* <FunctionComponent/> */}
+           <PropsComponent name='pooja' company='avalara'/>
         </h1>
-      </header>
+       
     </div>
   );
 }
