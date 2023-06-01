964392b8b140f12641aec564a382285879327f8f
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-01 16:12:34 +0530

    Update App.js

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -13,7 +13,7 @@ function App() {
            {/* <ClassComponent/> */}
            {/* <FunctionComponent/> */}
            {/* <PropsComponent name='pooja' company='avalara'/> */}
-           <PropsDestructureInClass/>
+           <PropsDestructureInClass name="pooja" company="Avalara"/>
         </h1>
        
     </div>
