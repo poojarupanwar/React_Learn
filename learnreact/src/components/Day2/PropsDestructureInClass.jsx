7bf77ff61e298ce22b2dc149798c2e3ab5f65b2a
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-01 15:21:00 +0530

    Create PropDestructureInClass.jsx
    Created props Destructure in Class

---
+++ learnreact/src/components/Day2/PropsDestructureInClass.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class PropsDestructureInClass extends Component {
+  render() {
+    return (
+      <div>PropsDestructureInClass</div>
+    )
+  }
+}
