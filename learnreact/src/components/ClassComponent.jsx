3a3659b305b62e2a077303dd7210ccaeba65230f
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-01 14:48:57 +0530

    classComponent.jsx
    Class component added

---
+++ learnreact/src/components/ClassComponent.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <>
+      <h1>ClassComponent</h1>
+      <h1>This is Class Component</h1>
+      </>
+    )
+  }
+}
