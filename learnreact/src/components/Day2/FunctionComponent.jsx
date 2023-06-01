99d12e3dbad89b5f4ec0b66e47406cb63e04a094
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-01 15:20:07 +0530

    Create FunctionComponent.jsx

---
+++ learnreact/src/components/Day2/FunctionComponent.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+//Function declaration syntax;
+
+// function FunctionComponents(){
+//     return(
+//         <>
+//         <h1>Hello World</h1>
+//         <h2>Functional componet in react</h2>
+//         </>
+//     )
+// }
+//---------------------functional expression synt--------------
+
+// const FunctionComponents=function(){
+//     return(
+//                 <>
+//                 <h1>Hello World</h1>
+//                 <h2>Functional componet in react</h2>
+//                 </>
+//             );
+// };
+
+// arrow function
+
+const FunctionComponents=()=>{
+    return(
+                        <>
+                        <h1>Hello World</h1>
+                        <h2>Functional componet in react</h2>
+                        </>
+                    );
+}
+
+export default FunctionComponents;
