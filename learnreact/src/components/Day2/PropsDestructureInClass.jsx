5b2705a7c3ca177a96d714cac5fa9c75c166cde9
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-01 15:53:42 +0530

    created PropDestructureClass.jsx

--- learnreact/src/components/Day2/PropsDestructureInClass.jsx
+++ learnreact/src/components/Day2/PropsDestructureInClass.jsx
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 
-export default class PropsDestructureInClass extends Component {
+class PropsDestructureInClass extends Component {
   render() {
+
+    //destructuring in class
+
+    const {name,company}=this.props;
+
     return (
-      <div>PropsDestructureInClass</div>
-    )
+        <>
+        <h1>Props Destructure In Class</h1>
+        <h1>
+            name : { name} 
+        </h1>
+        <h2>
+        Company : {company}
+        </h2>
+        </>
+        )
   }
 }
+export default PropsDestructureInClass;
