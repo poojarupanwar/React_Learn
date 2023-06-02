d72020142b80a9d70307179d7c21d0e4ed6936ea
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-03 00:02:51 +0530

    Update App.js

--- learnreact/src/App.js
+++ learnreact/src/App.js
@@ -1,20 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
 // import PropsComponent from './components/Day2/PropComponent';
-import PropsDestructureInClass from './components/Day2/PropsDestructureInClass';
+// import PropsDestructureInClass from './components/Day2/PropsDestructureInClass';
+import StateComp from './components/Day3/StateComp';
 // import FunctionComponent from './components/Day2/FunctionComponent';
 // import ClassComponent from './components/Day2/ClassComponent';
 
 function App() {
   return (
     <div className="App">
-         <h1>
+        
           hello
            {/* <ClassComponent/> */}
            {/* <FunctionComponent/> */}
            {/* <PropsComponent name='pooja' company='avalara'/> */}
-           <PropsDestructureInClass name="pooja" company="Avalara"/>
-        </h1>
+           {/* <PropsDestructureInClass name="pooja" company="Avalara"/> */}
+           <StateComp/>
+       
        
     </div>
   );
