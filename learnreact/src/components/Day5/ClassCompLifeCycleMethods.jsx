dab3adaf80f10944b4693e65255b218969b47ace
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-06 00:01:16 +0530

    Create ClassCompLifeCycle.jsx

---
+++ learnreact/src/components/Day5/ClassCompLifeCycleMethods.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class ClassCompLifeCycleMethods extends Component {
+  render() {
+    return (
+      <div>ClassCompLifeCycleMethods</div>
+    )
+  }
+}
