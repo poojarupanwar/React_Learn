48d4460d04231ef1ce1004b59f83e81e0533197b
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-06 00:01:30 +0530

    Create PureComp.jsx

---
+++ learnreact/src/components/Day7/PureComp.jsx
@@ -0,0 +1,39 @@
+import React, { PureComponent } from 'react'
+
+ class PureComp extends PureComponent {
+
+    constructor(props) {
+        console.log("constructor");
+      super(props)
+    
+      this.state = {
+         count:0
+      }
+    }
+
+    incrementCount=()=>{
+        this.setState({count: this.state.count+1})
+        // this.setState({count:this.state.count+1})
+    
+    }
+    
+    // incrementCount=() => {
+    //     console.log("incrementCount method called")
+    //    // this.setState({count:this.state.count+1})
+    //    this.setState({count:this.state.count})
+    // }
+  render() {
+    console.log('render()');
+    return (
+      <>
+      <h1>
+        PureComp
+      </h1>
+      <h3>Count: {this.state.count}</h3>
+
+      <button onClick={this.incrementCount}>Increnment count</button>
+      </>
+    )
+  }
+}
+export default PureComp;
