a85a5c8438cd8002dea42302b785950eb69f577a
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-07 00:44:22 +0530

    Update ClassCompLifeCycleMethods.jsx
    Update file

--- learnreact/src/components/Day5/ClassCompLifeCycleMethods.jsx
+++ learnreact/src/components/Day5/ClassCompLifeCycleMethods.jsx
@@ -1,9 +1,28 @@
 import React, { Component } from 'react'
 
-export default class ClassCompLifeCycleMethods extends Component {
+ class ClassCompLifeCycleMethods extends Component {
+ 
+ //---------- 1.Initialization phase--------------
+  constructor(props) {
+    console.log("constructor()");
+    super(props)
+  }
+
+  //----------2.Mounting phase--------------
+  static getDerivedStateFromProps(props,state){
+    console.log("In Mounting phase");
+  }
+  componentDidMount(){
+    console.log("In Component Did mount");
+  }
   render() {
+    console.log("render()")
     return (
-      <div>ClassCompLifeCycleMethods</div>
+      <>
+        <h1>Hello</h1 >
+       </>
     )
   }
 }
+
+export default ClassCompLifeCycleMethods;
