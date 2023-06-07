ca827e4155cb35807fda27d02da0d7a62cb158b4
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-07 15:01:36 +0530

    Update PropDrills.jsx

--- learnreact/src/components/Day8/PropDrills.jsx
+++ learnreact/src/components/Day8/PropDrills.jsx
@@ -1,43 +1,61 @@
 import React, { Component } from 'react'
 
-const MessageContext=React.createContext();
+class PropDrills extends Component {
  
- class PropDrills extends Component {
-constructor(props) {
-  super(props)
-
-  this.state = {
-     obj:{
-        message:"Welcome to react..!!",
-        name:'pooja rupanwar'
-     }
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+        message:'Welcome to the React ..!!!'
+    }
   }
-}
- render() {
-    return (
+  render() {
+        return (
       <>
-      <h1>Props Drilling</h1>
-      <MessageContext.Provider value={this.state.obj}>
-        <ChildA/>
-      </MessageContext.Provider>
+      <h1>Prop Drilling</h1>
+      <ChildA/>
       </>
     )
   }
 }
 
+
 export default PropDrills;
 
-
- class ChildA extends Component {
-  render() {
-    return (
+class ChildA extends React.Component{
+  render(){
+    return(
       <>
-     
-        <h1>I am a Child</h1>
-      <MessageContext.Consumer>
-              {( obj)=> <h2> {obj.message}</h2>}
-          </MessageContext.Consumer>
-   
+          <h4>
+            I am a ChildA component
+          </h4>
+          <ChildB/>
+      </>
+    )
+  }
+}
+
+
+class ChildB extends React.Component{
+  render(){
+    return(
+      <>
+          <h4>
+            I am a ChildB component
+          </h4>
+          <ChildC/>
+      </>
+    )
+  }
+}
+
+class ChildC extends React.Component{
+  render(){
+    return(
+      <>
+          <h4>
+            I am a ChildC component
+          </h4>
       </>
     )
   }
