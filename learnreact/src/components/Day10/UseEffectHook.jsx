0f477fce4ac0a863b159eeabf29f813ca7a9ce3b
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-07 14:34:51 +0530

    Create UseEffectHook.jsx

---
+++ learnreact/src/components/Day10/UseEffectHook.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+
+const UseEffectHook=()=> {
+    const[count,setCount]=useState(0)
+    const[name,setName]=useState("pooja")
+    // useeffect with no dependencies
+    useEffect(()=>{
+        console.log("useEffect -runs on every render");
+    })
+
+    useEffect(()=>{
+        console.log("useEffect -runs on First render");
+    },[])
+
+    useEffect(()=>{
+        console.log("useEffect -runs on First render");
+    },[name])
+  return (
+    <>
+    <h1>UseEffectHook</h1>
+    <h1>Count: {count}<button onClick={()=>setCount(count+1)}>+</button></h1>
+    <h1>Name: {name}<button onClick={()=>setName("kirti")}>Update Name</button></h1>
+    </>
+  )
+}
+
+export default UseEffectHook;
