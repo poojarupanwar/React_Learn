57a7d86cf96b65abc5266cb4418b57c90fcbdbc7
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-08 22:59:56 +0530

    Update UseEffectpiUse.jsx

--- learnreact/src/components/Day10/UseEffectApiUse.jsx
+++ learnreact/src/components/Day10/UseEffectApiUse.jsx
@@ -1,17 +1,51 @@
-import React, { useEffect } from 'react'
-
- function UseEffectApiUse() {
-    useEffect(()=>{
-        const getPostData=()=>{
-            try{
-                const pData=axios.get()
-            }catch(error){
-                console.log(error);
-            }
-        }
-    },[])
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import './postListStyle.css'
+function UseEffectApiCall() {
+  const [postData, setPostData] = useState([]);
+  useEffect(() => {
+    const getPostData = async () => {
+      try {
+        const pData = await axios.get(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+        console.log(pData.data);
+        setPostData(pData.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getPostData();
+  }, []);
   return (
-    <><h1>UseEffectApiUse</h1></>
-  )
+    <>
+      <div>
+        <h3>POST LIST</h3>
+      </div>
+      <div className="mrg25">
+        <table>
+          <thead>
+            <tr>
+              <th>USER ID </th>
+              <th>TITLE</th>
+              <th> BODY </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+               postData.map(post=>(
+                    <tr key={post.id}>
+                        <td>{post.userId}</td>
+                        <td>{post.title}</td>
+                        <td>{post.body}</td>
+                    </tr>
+               )) 
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 }
-export default UseEffectApiUse;
+
+export default UseEffectApiCall;
