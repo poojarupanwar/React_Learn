6fa93d5e7db6355def2e842d9ecd543b5ee473e0
poojarupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-06-17 13:40:27 +0530

    Update UncontrolledComp.jsx

--- learnreact/src/components/Day14/UncontrolledComp.jsx
+++ learnreact/src/components/Day14/UncontrolledComp.jsx
@@ -1,45 +1,58 @@
-import React, { createRef } from 'react'
+import React, { createRef } from "react";
 
 const UncontrolledComp = () => {
-    const userNameRef= React.createRef();
-    const passwordRef= React.createRef();
+  const userNameRef = React.createRef();
+  const passwordRef = React.createRef();
 
-    const handleLogin=(e)=>{
-        console.log(userNameRef.current.value);
-        console.log(passwordRef.current.value);
-        e.preventDefault();
-    }
+  const handleLogin = (e) => {
+    console.log(userNameRef.current.value);
+    console.log(passwordRef.current.value);
+    e.preventDefault();
+  };
 
   return (
-    <>  
-    <div style={{marginLeft:'150px' , marginRight:'150px'}}>
-    <h4>Login Form</h4>
-    <form onSubmit={handleLogin}>
-      <div className="row" >
-        <div className="six columns"  style={{ marginLeft:'300px'}}>
-          <label htmlfor="exampleEmailInput">Enter Username</label>
-          <input className="u-full-width" 
-          type="text" placeholder="Enter User Name" 
-          id="exampleEmailInput" ref={userNameRef} />
-        </div>
-        </div>
-        <div className="row">
-        <div className="six columns"  style={{ marginLeft:'300px'}}>
-        <label htmlfor="exampleEmailInput">Password</label>
-          <input className="u-full-width" 
-          type="password" placeholder="Enter your password" 
-          id="examplePasswordInput" ref={passwordRef}/>
-        </div>
-        
+    <>
+      <div style={{ marginLeft: "150px", marginRight: "150px" }}>
+        <h4>Login Form</h4>
+        <form onSubmit={handleLogin}>
+          <div className="row">
+            <div className="six columns" style={{ marginLeft: "300px" }}>
+              <label htmlfor="exampleEmailInput">Enter Username</label>
+              <input
+                className="u-full-width"
+                type="text"
+                placeholder="Enter User Name"
+                id="exampleEmailInput"
+                ref={userNameRef}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <div className="six columns" style={{ marginLeft: "300px" }}>
+              <label htmlfor="exampleEmailInput">Password</label>
+              <input
+                className="u-full-width"
+                type="password"
+                placeholder="Enter your password"
+                id="examplePasswordInput"
+                ref={passwordRef}
+              />
+            </div>
+          </div>
+          <div className="row">
+            <button
+              className="button-primary"
+              type="submit"
+              style={{ marginTop: "20px" }}
+              value="Submit"
+            >
+              Login
+            </button>
+          </div>
+        </form>
       </div>
-      <div className="row">
-        <button className="button-primary" type="submit"  style={{ marginTop:'20px'}} value="Submit">Login</button>
-        </div>
-    </form>
-    
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default UncontrolledComp
+export default UncontrolledComp;
